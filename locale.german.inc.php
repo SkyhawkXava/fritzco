e47079242cdcec9b1424c58e2e0187d241bfe93d
cbdreinulldrei <cb@dreinulldrei.de>
2013-11-12 11:38:12 +0100

    Create locale.german.inc.php
    German locale

---
+++ locale.german.inc.php
@@ -0,0 +1,47 @@
+<?php
+/*
+   @title German locale file for directory.php by Till Steinbach
+   @author Christian Bartsch <cb AT dreinulldrei DOT de>
+   @copyright (c) Till Steinbach
+   @license GPL v2
+   @date 2013-11-11
+*/
+ 
+ define ('PB_NAME_GENERAL', 'FRITZ!Box');
+ define ('PB_REFRESH', 'Aktualisieren');
+ define ('PB_LOGIN_FAILED', 'Login fehlgeschlagen');
+ define ('PB_ADMIN_CHECKPWD', 'Administrator: Bitte das ' . PB_NAME_GENERAL . '-Passwort in der config.php.inc überprüfen!');
+ define ('PB_APP_CALLSMISSED', 'Anrufe in Abwesenheit');
+ define ('PB_APP_CALLSRECEIVED', 'Angenommene Anrufe');
+ define ('PB_APP_CALLSPLACED', 'Ausgehende Anrufe');
+ define ('PB_FIELD_HOME', 'Privat');
+ define ('PB_FIELD_MOBILE', 'Mobil');
+ define ('PB_FIELD_WORK', 'Geschäftlich');
+ define ('PB_FIELD_FAX', 'Fax');
+ define ('PB_FIELD_FAX_WORK', 'Fax Geschäftlich');
+ define ('PB_FIELD_PRIVATE', 'Privat');
+ define ('PB_FIELD_BUSINESS', 'Geschäftlich');
+ define ('PB_FIELD_OTHER', 'Sonstige');
+ define ('PB_FIELD_EMAIL', 'eMail');
+ define ('PB_PHONEBOOK', 'Telefonbuch');
+ define ('PB_PHONEBOOKS', 'Telefonbücher');
+ define ('PB_SELECT_PHONEBOOK', 'Telefonbuch auswählen');
+ define ('PB_BUTTON_SELECT', 'Auswählen');
+ define ('PB_BUTTON_DIAL', 'Wählen');
+ define ('PB_BUTTON_DETAILS', 'Details'); 
+ define ('PB_BUTTON_REFRESH', 'Aktualisieren');
+ define ('PB_BUTTON_EXIT', 'Verlassen');
+ define ('PB_BUTTON_BACK', 'Zurück');
+ define ('PB_BUTTON_SEARCH', 'Suche'); 
+ define ('PB_BUTTON_PREVIOUS_PAGE', 'Vorherige Seite');
+ define ('PB_BUTTON_NEXT_PAGE', 'Nächste Seite');
+ define ('PB_NO_PHONEBOOKS', 'Keine Telefonbücher vorhanden');
+ define ('PB_NO_PHONEBOOKS_DESC', 'Es sind derzeit keine Telefonbücher vorhanden, durch "Aktualisieren" kann die ' . PB_NAME_GENERAL . ' ausgelesen werden. Dies kann einige Sekunden dauern! Nach Änderungen im FritzBox-Telefonbuch muss erneut das Aktualisieren ausgeführt werden.');
+ define ('PB_NO_ENTRIES', 'Keine Einträge vorhanden');
+ define ('PB_NO_ENTRIES_DESC', 'Das Adressbuch ist leer, oder keine Einträge entsprechen den Suchkriterien');
+ define ('PB_INPUT_QUERY', 'Bitte Namen oder Nummer eingeben');
+ define ('PB_INPUT_NAME', 'Name:');
+ define ('PB_INPUT_NUMBER', 'Nummer:');
+ define ('PB_NO_FURTHER_INFORMATION', 'Keine weiteren Informationen');
+ 
+?>
