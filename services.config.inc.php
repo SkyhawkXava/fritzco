9e8c33be2245930dc4ed9250fee68685581bb7dc
cbdreinulldrei <cb@dreinulldrei.de>
2013-11-17 10:52:32 +0100

    Create services.config.inc.php
    Initial release.

---
+++ services.config.inc.php
@@ -0,0 +1,13 @@
+<?
+/*
+ * @author Christian Bartsch <cb AT dreinulldrei DOT de>
+ * @copyright (c) Christian Bartsch
+ * @license GPL v2
+ * @date 2013-11-16
+ */
+
+$default_uid = 'cisco';
+$default_pass = 'phone';
+$default_ip = '192.168.1.20';
+		
+?>
