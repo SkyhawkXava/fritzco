82a80325bb4eee1b5b582d5e2df036aa7f20d4e8
cbdreinulldrei <cb@dreinulldrei.de>
2013-11-17 10:53:49 +0100

    Create quickdial.php
    Initial release.

---
+++ quickdial.php
@@ -0,0 +1,16 @@
+<?php  
+  header ("Content-Type:text/xml");  
+  echo ("<?xml version=\"1.0\" encoding=\"utf-8\"?>\n");
+?>
+<CiscoIPPhoneMenu>
+  <Title>Telefonbuch</Title>
+  <Prompt>Kurzwahlen</Prompt>
+    <MenuItem>
+      <Name>Mobilteil Wohnzimmer</Name>
+      <URL>Dial:**610</URL>
+    </MenuItem>
+    <MenuItem>
+      <Name>Mobilteil 1. Etage</Name>
+      <URL>Dial:**611</URL>
+    </MenuItem>
+</CiscoIPPhoneMenu>
