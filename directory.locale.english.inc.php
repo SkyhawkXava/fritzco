ba2b29b8e7a3452615bc9455ef309f3078b8113f
cbdreinulldrei <cb@dreinulldrei.de>
2013-11-17 10:41:48 +0100

    Create directory.locale.english.inc.php
    English locale.

---
+++ directory.locale.english.inc.php
@@ -0,0 +1,47 @@
+<?php
+/*
+   @title English locale file for directory.php by Till Steinbach
+   @author Christian Bartsch <cb AT dreinulldrei DOT de>
+   @copyright (c) Christian Bartsch
+   @license GPL v2
+   @date 2013-11-13
+*/
+ 
+ define ('PB_NAME_GENERAL', 'FRITZ!Box');
+ define ('PB_REFRESH', 'Refresh');
+ define ('PB_LOGIN_FAILED', 'Login failed');
+ define ('PB_ADMIN_CHECKPWD', 'Administrator: Please verify ' . PB_NAME_GENERAL . ' password in config.php.inc!');
+ define ('PB_APP_CALLSMISSED', 'Missed Calls');
+ define ('PB_APP_CALLSRECEIVED', 'Received Calls');
+ define ('PB_APP_CALLSPLACED', 'Calls Placed');
+ define ('PB_FIELD_HOME', 'Home');
+ define ('PB_FIELD_MOBILE', 'Cell');
+ define ('PB_FIELD_WORK', 'Work');
+ define ('PB_FIELD_FAX', 'Fax');
+ define ('PB_FIELD_FAX_WORK', 'Fax Work');
+ define ('PB_FIELD_PRIVATE', 'Private');
+ define ('PB_FIELD_BUSINESS', 'Business');
+ define ('PB_FIELD_OTHER', 'other');
+ define ('PB_FIELD_EMAIL', 'eMail');
+ define ('PB_PHONEBOOK', 'Phone book');
+ define ('PB_PHONEBOOKS', 'Phone books');
+ define ('PB_SELECT_PHONEBOOK', 'Select Phone book');
+ define ('PB_BUTTON_SELECT', 'Select');
+ define ('PB_BUTTON_DIAL', 'Dial');
+ define ('PB_BUTTON_DETAILS', 'Details'); 
+ define ('PB_BUTTON_REFRESH', 'Refresh');
+ define ('PB_BUTTON_EXIT', 'Exit');
+ define ('PB_BUTTON_BACK', 'Back');
+ define ('PB_BUTTON_SEARCH', 'Search'); 
+ define ('PB_BUTTON_PREVIOUS_PAGE', 'Previous Page');
+ define ('PB_BUTTON_NEXT_PAGE', 'Next Page');
+ define ('PB_NO_PHONEBOOKS', 'No phone books found');
+ define ('PB_NO_PHONEBOOKS_DESC', 'There are no phone books present. By pressing "Refresh" data will be read from ' . PB_NAME_GENERAL . '. This may take a couple of seconds. Please refresh from ' . PB_NAME_GENERAL . ' phone book after any changes made.');
+ define ('PB_NO_ENTRIES', 'No records found');
+ define ('PB_NO_ENTRIES_DESC', 'The address book is empty or no records match the search criteria');
+ define ('PB_INPUT_QUERY', 'Please enter name or number');
+ define ('PB_INPUT_NAME', 'Name');
+ define ('PB_INPUT_NUMBER', 'Number');
+ define ('PB_NO_FURTHER_INFORMATION', 'No further information');
+ 
+?>
