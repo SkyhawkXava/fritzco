121dae975ae2269cc16327b7122c5d3a624a30d8
cbdreinulldrei <cb@dreinulldrei.de>
2013-11-17 10:51:08 +0100

    Create services.locale.german.inc.php
    Initial release.

---
+++ services.locale.german.inc.php
@@ -0,0 +1,25 @@
+<?
+/*
+ * @author Christian Bartsch <cb AT dreinulldrei DOT de>
+ * @copyright (c) Christian Bartsch
+ * @license GPL v2
+ * @date 2013-11-16
+ */
+ 
+ define ('SERV_SERVICES_TITLE', 'Cisco IP Phone Dienste');
+ define ('SERV_PLEASE_CHOOSE', 'Bitte wählen');
+ define ('SERV_REBOOT_PHONE', 'Telefon neu starten');
+ define ('SERV_TESTCALL', 'Nummer wählen lassen');
+ define ('SERV_BUTTON_SELECT', 'Auswählen');
+ define ('SERV_BUTTON_EXIT', 'Verlassen');
+ define ('SERV_BUTTON_BACK', 'Zurück');
+ define ('SERV_TITLE_REBOOT', 'Cisco IP Phone neu starten');
+ define ('SERV_STATUS_REBOOT', 'Neustart von'); 
+ define ('SERV_TITLE_DIAL', 'Entferntes Wählen');  
+ define ('SERV_STATUS_NUMBERSENT', 'Nummer gesendet');  
+ define ('SERV_INPUT_QUERY', 'Nummer und Ziel eingeben'); 
+ define ('SERV_INPUT_TARGET', 'IP-Adresse');  
+ define ('SERV_INPUT_NUMBER', 'Rufnummer');  
+
+ 
+?>
