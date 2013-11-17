a57fba9e091717909ccb0a0564682e5e3d4893e8
cbdreinulldrei <cb@dreinulldrei.de>
2013-11-17 10:44:46 +0100

    Create directory.config.inc.php
    Configuration & passwords

---
+++ directory.config.inc.php
@@ -0,0 +1,22 @@
+<?php
+/*
+ * @author Till Steinbach <till.steinbach@gmx.de>
+ * @modified Christian Bartsch <cb AT dreinulldrei DOT de>
+ * @copyright (c) Till Steinbach
+ * @license GPL v2
+ */
+
+    $fritzbox_ip = 'fritz.box'; //Hier die IP oder den Hostname der FritzBox eintragen (meist fritz.box)
+    $fritzbox_password    = 'secret'; //Hier das Passwort eintragen
+	  $show_MissedCalls = false;
+    $show_ReceivedCalls = false;
+    $show_PlacedCalls = false;
+	
+	$show_QuickDial = true;
+	define ('QUICKDIAL_URL', 'http://intrasrv.fritz.box/cisco/quickdial.php'); // Pfad zu separatem Telefonbuch mit eigenen, festen internen Nummern.
+	define ('QUICKDIAL_NAME', 'Kurzwahlen');
+	
+	$runon_Fritzbox = false; // Wenn Script direkt auf modifizierter FB eingesetzt wird, kann das erste Telefonbuch lokal kopiert werden
+	define ('FRITZBOX_LOCAL_PATH', '/var/media/ftp/###STICKNAME###/###PATH###/0.xml'); // Pfad anpassen mit Stickname und Zielordner.
+	 
+?>
