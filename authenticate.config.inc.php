36318fcfda8c43865960028814e11a5d94362c0d
cbdreinulldrei <cb@dreinulldrei.de>
2013-11-17 10:48:11 +0100

    Create authenticate.config.inc.php
    Phone access credentials are placed here.

---
+++ authenticate.config.inc.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * @title Simple Cisco Auth Manager
+ * @author Christian Bartsch <cb AT dreinulldrei DOT de>
+ * @copyright (c) Christian Bartsch
+ * @license GPL v2
+ * @date 2013-11-16
+ */
+
+ // Configuration; add devices with user and password here
+ 
+ $authdata = array( array( 	'devicename' => 'SEP00123456789',
+							'UserID' => 'cisco', 
+							'Password' => 'phone',
+                       
+                    ),
+				array( 	'devicename' => 'SEP00987654321',
+							'UserID' => 'cisco', 
+							'Password' => 'phone',
+                       
+                    )
+             );
+?>
