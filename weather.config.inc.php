e86fec92af9c612b43124873ca57ae44d7a395b9
cbdreinulldrei <cb@dreinulldrei.de>
2013-12-06 22:13:47 +0100

    Create weather.config.inc.php

---
+++ weather.config.inc.php
@@ -0,0 +1,18 @@
+<?php
+/*
+ * @author Christian Bartsch <cb AT dreinulldrei DOT de>, bt43a
+ * @copyright (c) Christian Bartsch, bt43a
+ * @license GPL v2
+ * @date 2013-12-06
+ */
+	$weather_city = 'Berlin';
+	$weather_id = ; // can be omitted, unless there are several matches for your city by name
+	$lang = 'de';
+	$units = 'metric';
+	$gmt_offset = 1; // adjust for your timezone
+	$target = '79x5'; // currently supports 79x5 and 99xx (standard), affects graphics resolution
+ 
+	$wallpaper_path = 'weather/wallpaper/';
+	$wallpaper_file = 'XXXbyYYY_whatever.png'; // should match 498x289 for 99x, 289x168 or 298x156 for 79xx phones
+
+?>
